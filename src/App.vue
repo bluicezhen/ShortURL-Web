3394b596c9db2638358addc2a657959f3fd38426
Bluice Zhen <bluice.zhen@gmail.com>
2017-11-03 12:03:41 +0800

    cpmplete home view

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,10 @@
     padding: 0;
     font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   }
+  // To remove default <input>'s inner shadow for IOS dev
+  input {
+    -webkit-appearance: none;
+  }
   #app {
     display: flex;
     justify-content: space-between;
--- src/page/Home.vue
+++ src/page/Home.vue
@@ -1,4 +1,90 @@
 <template>
   <div id="su-home">
+    <div class="input">
+      <div class="form">
+        <div class="i">
+          <input type="text" placeholder="Input full URL here, like: https://www.google.com"/>
+        </div>
+        <div class="b">
+          <button>Make Short</button>
+        </div>
+      </div>
+    </div>
+    <div class="result">
+
+    </div>
   </div>
 </template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss">
+  #su-home {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    .input {
+      flex-grow: 1;
+      display: flex;
+      .form {
+        margin: auto;
+        div {
+          float: left;
+        }
+        input {
+          width: 400px;
+          height: 30px;
+          padding: 0 0 0 5px;
+          border: 1px solid #dadada;
+          border-right: 0;
+          border-radius: 0; // For IOS
+        }
+        input:focus {
+          outline-offset: 0;
+          outline: none;
+        }
+        button {
+          height: 32px;
+          background-color: #fff;
+          border: 1px solid #dadada;
+          margin: 0; // For Safari
+        }
+        button:focus {
+          outline-offset: 0;
+          outline: none;
+        }
+      }
+    }
+    .result {
+      flex-grow: 1;
+    }
+  }
+
+  @media screen and (max-width: 1000px) {
+    #su-home {
+      .input {
+        .form {
+          display: flex;
+          justify-content: space-between;
+          width: 90%;
+          margin: auto;
+          .i {
+            flex-grow: 2;
+            input {
+              width: 100%;
+            }
+          }
+          .b {
+            button {
+              width: 100px;
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
